45c276666fe068d5bd274edff3aeb9f5edd48a71
Santhosh G S <santhoshgs013@gmail.com>
2023-10-02 13:30:36 +0530

    Completed the second problem of part 1

---
+++ Part 1/2 Square, Cube and 2^n/sqr_cube_2^n.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number : "))
+
+sqr = n**2
+cube = n**3
+two_pow_n = 2**n
+
+print("The square of the number is", sqr)
+print("The cube of the number is", cube)
+print("Two power the number is", two_pow_n)
