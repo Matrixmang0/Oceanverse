4de0bee53dffbac0706ecef6703583662f8c5846
Santhosh G S <santhoshgs013@gmail.com>
2023-10-06 23:04:08 +0530

    Completed 30th question of part 2

--- Part 2 /30 Euclidian Algorithm/euclidian_algorithm.py
+++ Part 2 /30 Euclidian Algorithm/euclidian_algorithm.py
@@ -1,30 +1,35 @@
-def fibonacci_k(k):
+def euclids_algo(a, b, steps=0):
+
+  if (b>a):
+    (a, b) = (b, a)
+  if (b==0):
+    return (a,steps)
+  else:
+    return euclids_algo(b, a%b, steps+1)
+  
+def seq_fibo(k):
+
   num1 = 0
   num2 = 1
 
-  li = [num2]
+  li = []
+
+  while(1):
+    if (len(str(num1))==k) and (len(str(num2))==k):
+      li = [num1, num2]
+    elif (len(str(num1))==k) and (len(str(num2))==k+1):
+      return li
+    
+    temp = num1
+    num1 = num2
+    num2 += temp
+
+k = int(input("Enter the number of digits : "))  
+
+dig1, dig2 = seq_fibo(k)[0], seq_fibo(k)[1]
+
+(gcd, steps) = euclids_algo(dig1, dig2)
+
+print("The two digits of length k are "+str(dig1)+" and "+str(dig2))
+print("Their GCD is", gcd,"and number of steps taken is", steps)
   
-  while(str(num2<=k)):
-    temp = num2
-    num2 += num1
-    num1 = temp
-    if (str(num2<=k)):
-      li.append(num2)
-
-  return li
-
-
-def euclid_gcd(m, n):
-  if (n > m):
-    (m, n) = (n, m)
-  steps = 0
-  while(n > 0):
-    temp = m%n
-    m = n
-    n = temp
-    if (n > m):
-      (m, n) = (n, m)
-    steps += 1
-  return (m, steps)
-
-print(fibonacci_k(1)) 
