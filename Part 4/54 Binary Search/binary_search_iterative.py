d88e28db4dc270a74a98bd63abcf98a8c3ce8579
Santhosh G S <santhoshgs013@gmail.com>
2023-10-20 01:15:04 +0530

    Completed 54th problem in iterative method

--- Part 4/54 Binary Search/binary_search_iterative.py
+++ Part 4/54 Binary Search/binary_search_iterative.py
@@ -1,19 +1,18 @@
 def BinarySearch(li, key):
   l = 0
-  h = len(li)-1
+  h = len(li)
   while(l<=h):
     mid = (l+h)//2
     if  li[mid]==key:
       return True
-    if li[mid]>key:
-      h = mid
+    elif li[mid]>key:
+      h = mid-1
     elif li[mid]<key:
-      l = mid
+      l = mid+1
   return False
 
 
 li1 = sorted([5, 67, 8, 90, 12, 123, 78])
 li2 = sorted([6, 34, 78, 11, 334, 70, 24])
 li3 = list(range(0, 50))
-print(li1)
-print(BinarySearch(li1, 75))
+print(BinarySearch(li1, 65))
