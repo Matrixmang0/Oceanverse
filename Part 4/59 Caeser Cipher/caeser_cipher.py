2744f904f20b430d462ae3a8328f29cd11c5866c
Santhosh G S <santhoshgs013@gmail.com>
2023-11-06 23:33:55 +0530

    Added comments and docstring to caesar cipher

--- Part 4/59 Caeser Cipher/caeser_cipher.py
+++ Part 4/59 Caeser Cipher/caeser_cipher.py
@@ -1,52 +1,81 @@
+# Define the lowercase and uppercase alphabets
 alpha = 'abcdefghijklmnopqrstuvwxyz'
 Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# Define special characters
 spl_chr = '[@_!#$%^&*,.()<>?/\|}{~:;+-] '
 
 def caeser_encrypt(string, key):
+    """
+    Encrypt a string using the Caesar cipher with a given key.
 
-  new_str = ""
+    Args:
+        string (str): The input string to be encrypted.
+        key (int): The encryption key.
 
-  for i in range(len(string)) :
-    if string[i] not in spl_chr:
-      if string[i].isupper():
-        ind = Alpha.index(string[i])
-        newind = (ind + key)%26
-        new_str += Alpha[newind]
-      else:
-        ind = alpha.index(string[i])
-        newind = (ind + key)%26
-        new_str += alpha[newind]
-    else:
-      new_str += string[i]
+    Returns:
+        str: The encrypted string.
+    """
+    new_str = ""
 
-  return new_str
+    # Iterate through each character in the input string
+    for i in range(len(string)):
+        # Check if the character is not a special character
+        if string[i] not in spl_chr:
+            # Check if the character is uppercase
+            if string[i].isupper():
+                ind = Alpha.index(string[i])
+                newind = (ind + key) % 26  # Apply the Caesar cipher shift
+                new_str += Alpha[newind]
+            else:
+                ind = alpha.index(string[i])
+                newind = (ind + key) % 26  # Apply the Caesar cipher shift
+                new_str += alpha[newind]
+        else:
+            # Keep special characters unchanged
+            new_str += string[i]
+
+    return new_str
 
 def caeser_decrypt(string, key):
+    """
+    Decrypt a string encrypted using the Caesar cipher with a given key.
 
-  new_str = ""
+    Args:
+        string (str): The encrypted string to be decrypted.
+        key (int): The decryption key.
 
-  for i in range(len(string)) :
-    if string[i] not in spl_chr:
-      if string[i].isupper():
-        ind = Alpha.index(string[i])
-        newind = (ind - key)%26
-        new_str += Alpha[newind]
-      else:
-        ind = alpha.index(string[i])
-        newind = (ind - key)%26
-        new_str += alpha[newind]
-    else:
-      new_str += string[i]
+    Returns:
+        str: The decrypted string.
+    """
+    new_str = ""
 
-  return new_str
+    # Iterate through each character in the input string
+    for i in range(len(string)):
+        # Check if the character is not a special character
+        if string[i] not in spl_chr:
+            # Check if the character is uppercase
+            if string[i].isupper():
+                ind = Alpha.index(string[i])
+                newind = (ind - key) % 26  # Apply the Caesar cipher reverse shift
+                new_str += Alpha[newind]
+            else:
+                ind = alpha.index(string[i])
+                newind = (ind - key) % 26  # Apply the Caesar cipher reverse shift
+                new_str += alpha[newind]
+        else:
+            # Keep special characters unchanged
+            new_str += string[i]
 
+    return new_str
 
+# Example sentence to be encrypted and decrypted
 sentence = "Hello, how are you?"
 
+# Encrypt the sentence with a key of 4
 encrypted = caeser_encrypt(sentence, 4)
-print(encrypted)
+print("Encrypted:", encrypted)
 
+# Decrypt the encrypted sentence with the same key of 4
 decrypted = caeser_decrypt(encrypted, 4)
-print(decrypted)
-
+print("Decrypted:", decrypted)
