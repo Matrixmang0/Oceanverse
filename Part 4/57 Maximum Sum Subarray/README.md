6193a40db46bb111a38b55215ae820e0588d4ef3
Santhosh G S <santhoshgs013@gmail.com>
2023-11-07 23:45:12 +0530

    Added comments to kadane's algo and added README file for it

---
+++ Part 4/57 Maximum Sum Subarray/README.md
@@ -0,0 +1,20 @@
+# Maximum Subarray Sum with Kadane's Algorithm
+
+This Python script implements Kadane's algorithm to find the maximum subarray sum within a given list of integers.
+
+## Table of Contents
+
+- [Description](#description)
+- [Usage](#usage)
+
+## Description
+
+Kadane's algorithm is a popular method for efficiently finding the maximum subarray sum within an array of numbers. This script provides a Python implementation of the algorithm and returns both the maximum sum and the corresponding subarray.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Matrixmang0/Oceanverse.git
+   ```
--- Part 4/57 Maximum Sum Subarray/kadane's_algo.py
+++ Part 4/57 Maximum Sum Subarray/kadane's_algo.py
@@ -1,24 +1,38 @@
 import numpy as np
 
 def kadane_algo(array):
+    """
+    Find the maximum subarray sum using the Kadane's algorithm.
 
-  max_sum = -np.inf
-  curr_sum = 0
-  start = 0
-  end = 0
-  for i in range(len(array)):
-    curr_sum = max(array[i], curr_sum + array[i])
-    if curr_sum==array[i]:
-      start = i
-    max_sum = max(curr_sum, max_sum)
-    if curr_sum==max_sum:
-      end = i
+    Args:
+        array (list): A list of integers.
 
-  return (max_sum, array[start:end+1])
+    Returns:
+        tuple: A tuple containing the maximum subarray sum and the subarray itself.
+    """
+    max_sum = -np.inf  # Initialize the maximum sum to negative infinity
+    curr_sum = 0      # Initialize the current sum to 0
+    start = 0         # Initialize the start index of the maximum subarray
+    end = 0           # Initialize the end index of the maximum subarray
 
+    for i in range(len(array)):
+        curr_sum = max(array[i], curr_sum + array[i])
+
+        # Update the start index if a new subarray with a higher sum is found
+        if curr_sum == array[i]:
+            start = i
+
+        max_sum = max(curr_sum, max_sum)
+
+        # Update the end index if a new maximum subarray is found
+        if curr_sum == max_sum:
+            end = i
+
+    return (max_sum, array[start:end+1])
 
 li = [-5, -43, -9, -5, -44, -54, -1, -7, 89, -6, -55]
 
+# Apply the Kadane's algorithm to find the maximum subarray sum and subarray
 (summ, sub_array) = kadane_algo(li)
 
 print("The maximum sum is ", summ)
